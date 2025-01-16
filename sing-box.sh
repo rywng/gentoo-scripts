6a81307b50bc11323487172cf9676d2e5f40dba5
root <root@null.io>
2025-01-16 11:21:13 +0800

    Use set -e

--- sing-box.sh
+++ sing-box.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+set -e
+
 choice=$(ls /usr/local/etc/sing-box/configs/ | fzf --no-multi)
 if test -n "$choice"; then
-	echo $choice | xargs -I {} cp /usr/local/etc/sing-box/configs/{} /usr/local/etc/sing-box/config.json &&
-		sing-box check -c /usr/local/etc/sing-box/config.json &&
-		systemctl restart sing-box.service &&
+	echo $choice | xargs -I {} cp /usr/local/etc/sing-box/configs/{} /usr/local/etc/sing-box/config.json
+		sing-box check -c /usr/local/etc/sing-box/config.json
+		systemctl restart sing-box.service
 		systemctl restart unbound.service
 fi
