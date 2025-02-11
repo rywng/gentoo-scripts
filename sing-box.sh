dc13fa4feef3192e58d17d6e921f8cbd74678ded
root <root@null.io>
2025-02-11 15:28:57 +0800

    Make sing box config modular

--- sing-box.sh
+++ sing-box.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-choice=$(ls /usr/local/etc/sing-box/configs/ | fzf --no-multi)
-if test -n "$choice"; then
-	echo $choice | xargs -I {} cp /usr/local/etc/sing-box/configs/{} /usr/local/etc/sing-box/config.json
-		sing-box check -c /usr/local/etc/sing-box/config.json
-		systemctl restart sing-box.service
-		systemctl restart unbound.service
+_output_path="/usr/local/etc/sing-box/config.json"
+_prefix="/usr/local/etc/sing-box/configs"
+
+inbound=$(ls $_prefix/inbound | fzf --no-multi)
+outbound=$(ls $_prefix/outbound | fzf --no-multi)
+if test -n "$inbound" -a -n "$outbound" ; then
+	jq -s ".[0] * .[1] * .[2]" $_prefix/base.json $_prefix/inbound/$inbound $_prefix/outbound/$outbound > $_output_path
+	sing-box check -c /usr/local/etc/sing-box/config.json
+	systemctl restart sing-box.service
+	systemctl restart unbound.service
 fi
